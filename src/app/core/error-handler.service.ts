1761cbf530e23143d1f06368add74ad416f44e2d
Jonatan <jonatanfreitasc@gmail.com>
2022-11-13 16:50:17 -0300

    recuperando mensagem de erro fornecida pelo back end

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { MessageService } from 'primeng/api';
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,13 @@ export class ErrorHandlerService {
     let msg: string;
     if(typeof errorResponse === 'string'){
       msg=errorResponse;
-    } else{
+    }else if (errorResponse instanceof HttpErrorResponse && errorResponse.status >= 400 && errorResponse.status <= 499) {
+        msg = 'Ocorreu um erro ao processar a sua solicitação';
+        try {
+          msg = errorResponse.error[0].mensagemUsuario;
+        } catch (e) { }
+        console.error('Ocorreu um erro', errorResponse);
+    }else{
       msg='Erro ao processar serviço remoto. Tente novamente.';
       console.log('Ocorreu um erro',errorResponse);
     }
