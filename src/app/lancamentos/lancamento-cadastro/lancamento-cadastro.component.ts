59551a2b100e44c07e04de9d4154f6da9293c838
Jonatan <jonatanfreitasc@gmail.com>
2022-11-15 00:15:03 -0300

    preenchendo combo com dados
    preenchendo combo com dados

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,3 +1,6 @@
+import { PessoaService } from './../../pessoas/pessoa.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { CategoriaService } from './../../categorias/categoria.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,27 +8,47 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './lancamento-cadastro.component.html',
   styleUrls: ['./lancamento-cadastro.component.css']
 })
-export class LancamentoCadastroComponent  {
+export class LancamentoCadastroComponent  implements OnInit{
   tipos=[
           {label: 'Receita', value: 'RECEITA'},
           {label: 'Despesa', value: 'DESPESA'}
         ]
-  categorias=[
-          {label: 'Alimentação', value: '1'},
-          {label: 'Transporte', value: '2'},
-          {label: 'Educação', value: '3'},
-          {label: 'Medico', value: '4'},
-          {label: 'Academias', value: '5'}
-        ]
-        pessoas=[
-          {label: 'Jonatan', value: '1'},
-          {label: 'Sara', value: '2'},
-          {label: 'Rafael', value: '3'},
-          {label: 'Edio', value: '4'},
-          {label: 'Neide', value: '5'},
-          {label: 'Messias', value: '6'}
-        ]
+  categorias: any[] = [];
+          // {label: 'Alimentação', value: '1'},
+          // {label: 'Transporte', value: '2'},
+          // {label: 'Educação', value: '3'},
+          // {label: 'Medico', value: '4'},
+          // {label: 'Academias', value: '5'}
+
+        pessoas: any[] = [];
+          // {label: 'Jonatan', value: '1'},
+          // {label: 'Sara', value: '2'},
+          // {label: 'Rafael', value: '3'},
+          // {label: 'Edio', value: '4'},
+          // {label: 'Neide', value: '5'},
+          // {label: 'Messias', value: '6'}
 
 
+constructor(private categoriaService:CategoriaService,
+            private pessoaService:PessoaService,
+            private errorHandler: ErrorHandlerService){}
+ngOnInit(): void {
+  this.carregarCategorias();
+  this.carregarPessoas();
+}
+carregarCategorias(){
+  return this.categoriaService.listarTodas()
+  .then(categorias => {
+    this.categorias = categorias.map( (c:any) => ({label: c.nome, value: c.codigo}))
+  })
+  .catch(erro => this.errorHandler.handle(erro));
+}
+carregarPessoas(){
+  return this.pessoaService.listarTodas()
+  .then(pessoas => {
+    this.pessoas = pessoas.map( (c:any) => ({label: c.nome, value: c.codigo}))
+  })
+  .catch(erro => this.errorHandler.handle(erro));
+}
 
 }
