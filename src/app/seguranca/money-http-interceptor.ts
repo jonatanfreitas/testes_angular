943b2c29fdf672bc6bab4260f3cd6a49f273c62b
Jonatan <jonatanfreitasc@gmail.com>
2022-11-26 15:41:33 -0300

    verificando validade do Refresh Token

--- src/app/seguranca/money-http-interceptor.ts
+++ src/app/seguranca/money-http-interceptor.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-
 import { Observable, from } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
-
 import { AuthService } from './auth.service';
 
+export class NotAuthenticatedError {}
 @Injectable()
 export class MoneyHttpInterceptor implements HttpInterceptor {
 
@@ -16,6 +15,9 @@ export class MoneyHttpInterceptor implements HttpInterceptor {
       return from(this.auth.obterNovoAccessToken())
         .pipe(
           mergeMap(() => {
+            if (this.auth.isAccessTokenInvalido()) {
+              throw new NotAuthenticatedError();
+            }
             req = req.clone({
               setHeaders: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
