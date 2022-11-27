609700e1859eb8de7b6e522057f977704c9ccb7f
Jonatan <jonatanfreitasc@gmail.com>
2022-11-27 18:01:01 -0300

    alterando apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://financeiroteste-angular.herokuapp.com:8080',
+  apiUrl: 'https://financeiroteste-api.herokuapp.com:8080',
   // apiUrl: 'http://localhost:8080',
   // tokenAllowedDomains: [  /localhost:8080/ ],
   tokenAllowedDomains: [ /financeiroteste-angular.herokuapp.com/ ],
