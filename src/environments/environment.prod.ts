48f1e0d224f97b04558c99d1df766a7a0e121550
Jonatan <jonatanfreitasc@gmail.com>
2022-11-27 11:29:24 -0300

    ajuste de variaveis do package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://financeiroteste-ui.herokuapp.com:8080',
-  tokenAllowedDomains: [ /financeiroteste-ui.herokuapp.com/ ],
+
+  apiUrl: 'https://financeiroteste-angular.herokuapp.com:8080',
+  // apiUrl: 'http://localhost:8080',
+  // tokenAllowedDomains: [  /localhost:8080/ ],
+  tokenAllowedDomains: [ /financeiroteste-angular.herokuapp.com/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
